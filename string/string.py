5d8868c9b700b28fff19b0f2b58e9acc42b4df11
cppascalinux <cppascalinux@gmail.com>
2019-03-30 22:50:59 +0800

    string

---
+++ string/string.py
@@ -0,0 +1,8 @@
+s='ggn天下第一'
+print('s is %s the length of the string is %d'%(s,len(s)))
+bt=s.encode('utf-8')
+print('bt is %s the number of bytes is %d'%(bt,len(bt)))
+s1=72
+s2=85
+r=(s2-s1)/s1
+print('%f'%((s2-s1)/s1))
