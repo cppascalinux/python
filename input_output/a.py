b311fdec58f361cf018332fe07948503dd15d64f
cppascalinux <cppascalinux@gmail.com>
2019-03-30 22:28:04 +0800

    first

---
+++ input_output/a.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+print("hello world! what's your name?")
+a=input()
+print("1024*768=",1024*768)
+print("your name is",a)
+print("hello\nmy name is\n",a)
+print("""hello
+i
+am
+superman""")
